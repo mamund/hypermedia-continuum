942b901b6056fa5a25e23c46bbaaf9e7351f5cc8
Mike Amundsen <mamund@yahoo.com>
2012-09-20 18:10:59 -0400

    update app.js

--- nodejs/data/app.js
+++ nodejs/data/app.js
@@ -1,14 +1,21 @@
 /* data-only server */
 
 var http = require('http');
-var fs = require('fs');
-var querystring = require('querystring');
-
+var todo = require('./todo.js');
 var port = (process.env.PORT||1337);
 
 function handler(req, res) {
-    res.writeHead(200);
-    res.end('handler');
+    var item = {};
+    item.title = 'this is a test';
+    item.done = false;
+
+    list = todo('add',item);
+    sendResponse(req, res, rtn);
+}
+
+function sendResponse(req, res, list) {
+    res.writeHead(200,'ok',{'content-type':'application/json'});
+    res.end(JSON.stringify(list));
 }
 
 http.createServer(handler).listen(port);
